d2a4aafb8e89fa96d891f30e9dd5a497c8ad0ac2
Julian Tith <julian.tith@gmail.com>
2012-06-03 12:37:29 -0700

    Add links for CSS & add TODO
    Add links for CSS and create comment to not forget to uncomment
    setInterval so the lobby can update itself.

--- lobby.php
+++ lobby.php
@@ -6,7 +6,9 @@ and open the template in the editor.
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Lobby</title>
+        <link rel="stylesheet" type="text/css" href="CSS/base.css"/>
+        <link rel="stylesheet" type="text/css" href="CSS/lobby.css"/>
     </head>
     <body>
         <?php
@@ -19,15 +21,16 @@ and open the template in the editor.
             echo 'The room has closed.';
         }
         echo '<a href="logout.php">Logout</a></br>';
-        echo '<a href="create.php">Create Room</a></br>';
+        echo '<a href="create.php">Create Room</a></br></br>';
         ?>
-        <div id="lobbylist"><?php include 'lobby_list.php' ?></div>
+        <div id="lobbyList"><?php include 'lobby_list.php' ?></div>
         
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
         <script type="text/javascript" src="JS/loadLobby.js"></script>
         <script type="text/javascript">
             $(document).ready(function(){
-                setInterval(loadLobby, 1000);
+                //TODO: Don't forget to uncomment for lobby updates
+                //setInterval(loadLobby, 1000);
             });
         </script>
     </body>
