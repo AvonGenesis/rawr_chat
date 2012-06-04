b92d06a259b0ef97d2fa42852552be3095da1651
Julian Tith <julian.tith@gmail.com>
2012-06-04 16:39:47 -0700

    Add new noty for lobby

--- js/notyContainer.js
+++ js/notyContainer.js
@@ -21,3 +21,11 @@ function successCreateUser(){
 function errorUserAlreadyExist() {
     noty({"text":"Username is already in use!","layout":"top","type":"error","animateOpen":{"height":"toggle"},"animateClose":{"height":"toggle"},"speed":500,"timeout":5000,"closeButton":false,"closeOnSelfClick":true,"closeOnSelfOver":false,"modal":false});
 }
+
+function successLogin(){
+    noty({"text":"Successfully logged in.","layout":"top","type":"success","animateOpen":{"height":"toggle"},"animateClose":{"height":"toggle"},"speed":500,"timeout":5000,"closeButton":false,"closeOnSelfClick":true,"closeOnSelfOver":false,"modal":false});
+}
+
+function infoRoomClose(){
+    noty({"text":"The room you were in has been closed.","layout":"top","type":"information","animateOpen":{"height":"toggle"},"animateClose":{"height":"toggle"},"speed":500,"timeout":5000,"closeButton":false,"closeOnSelfClick":true,"closeOnSelfOver":false,"modal":false});
+}
