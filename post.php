e7a0613d721df334c4a0d376f8ab8c3589efa89d
Julian Tith <julian.tith@gmail.com>
2012-06-03 23:34:01 -0700

    Fix require_once file location name

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'PHP/DB/chatrooms.class.php';
+require_once 'php/db/chatrooms.class.php';
 session_start();
 if(isset($_SESSION['username'])){
 	$message = mysql_real_escape_string($_POST['text']);
