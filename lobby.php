432ab40723670bc931f3f71fe06b7f70c6eed124
Julian Tith <julian.tith@gmail.com>
2012-06-04 16:39:36 -0700

    Add code to handle login success message

--- lobby.php
+++ lobby.php
@@ -1,7 +1,3 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,6 +5,12 @@ and open the template in the editor.
         <title>Lobby</title>
         <link rel="stylesheet" type="text/css" href="css/base.css"/>
         <link rel="stylesheet" type="text/css" href="css/lobby.css"/>
+        <link rel="stylesheet" type="text/css" href="css/jquery.noty.css"/>
+        <link rel="stylesheet" type="text/css" href="css/noty_theme_default.css"/>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>  
+        <script type="text/javascript" src="js/loadLobby.js"></script>
+        <script type="text/javascript" src="js/jquery.noty.js"></script>
+        <script type="text/javascript" src="js/notyContainer.js"></script>
     </head>
     <body>
         <?php
@@ -17,24 +19,25 @@ and open the template in the editor.
         session_start();            
         $roomID = $_SESSION['roomID'];
         $username = $_SESSION['username'];
-        if(isset($_GET['lobby'])){
+        if (isset($_GET['lobby'])){
             Chatrooms::postMessage($username . ' has left the chatroom.', $roomID);
+            $_SESSION['chatID'] = null;
             Users::setRoomID(null);
         }
-        if(isset($_GET['roomClosed'])){
-            echo 'The room has closed.';
+        if (isset($_GET['login'])){
+            echo '<script type="text/javascript">successLogin()</script>';
+        }
+        if (isset($_GET['roomClosed'])){
+            echo '<script type="text/javascript">infoRoomClose()</script>';
         }
         echo '<a href="logout.php">Logout</a></br>';
         echo '<a href="create.php">Create Room</a></br></br>';
         ?>
         <div id="lobbyList"><?php include 'lobby_list.php' ?></div>
-        
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
-        <script type="text/javascript" src="js/loadLobby.js"></script>
         <script type="text/javascript">
             $(document).ready(function(){
                 //TODO: Don't forget to uncomment for lobby updates
-                //setInterval(loadLobby, 1000);
+                setInterval(loadLobby, 1000);
             });
         </script>
     </body>
