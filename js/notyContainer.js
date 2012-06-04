d8b311d1996b642f51379e14de5d570e20638d3b
Julian Tith <julian.tith@gmail.com>
2012-06-03 23:24:41 -0700

    Add files for toaster notifications

---
+++ js/notyContainer.js
@@ -0,0 +1,23 @@
+function errorLogin() {
+    noty({"text":"The username and password you entered do not match!","layout":"top","type":"error","animateOpen":{"height":"toggle"},"animateClose":{"height":"toggle"},"speed":500,"timeout":5000,"closeButton":false,"closeOnSelfClick":true,"closeOnSelfOver":false,"modal":false});
+}
+
+function errorUsername() {
+    noty({"text":"Please enter a username!","layout":"top","type":"error","animateOpen":{"height":"toggle"},"animateClose":{"height":"toggle"},"speed":500,"timeout":5000,"closeButton":false,"closeOnSelfClick":true,"closeOnSelfOver":false,"modal":false});
+}
+
+function errorPasswordNotLongEnough() {
+    noty({"text":"Password must be at least 4 characters long!","layout":"top","type":"error","animateOpen":{"height":"toggle"},"animateClose":{"height":"toggle"},"speed":500,"timeout":5000,"closeButton":false,"closeOnSelfClick":true,"closeOnSelfOver":false,"modal":false});
+}
+
+function errorPasswordDoNotMatch() {
+    noty({"text":"The password you entered do not match!","layout":"top","type":"error","animateOpen":{"height":"toggle"},"animateClose":{"height":"toggle"},"speed":500,"timeout":5000,"closeButton":false,"closeOnSelfClick":true,"closeOnSelfOver":false,"modal":false});
+}
+
+function successCreateUser(){
+    noty({"text":"User has been successfully created, you may now log in.","layout":"top","type":"success","animateOpen":{"height":"toggle"},"animateClose":{"height":"toggle"},"speed":500,"timeout":5000,"closeButton":false,"closeOnSelfClick":true,"closeOnSelfOver":false,"modal":false});
+}
+
+function errorUserAlreadyExist() {
+    noty({"text":"Username is already in use!","layout":"top","type":"error","animateOpen":{"height":"toggle"},"animateClose":{"height":"toggle"},"speed":500,"timeout":5000,"closeButton":false,"closeOnSelfClick":true,"closeOnSelfOver":false,"modal":false});
+}
