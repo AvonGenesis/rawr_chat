2131d06b93300446a52a8a0a87b78bd21c631ba3
Julian Tith <julian.tith@gmail.com>
2012-06-03 23:36:28 -0700

    Fix require_once file location name & remove extra GET for roomClosed

--- lobby.php
+++ lobby.php
@@ -7,13 +7,13 @@ and open the template in the editor.
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Lobby</title>
-        <link rel="stylesheet" type="text/css" href="CSS/base.css"/>
-        <link rel="stylesheet" type="text/css" href="CSS/lobby.css"/>
+        <link rel="stylesheet" type="text/css" href="css/base.css"/>
+        <link rel="stylesheet" type="text/css" href="css/lobby.css"/>
     </head>
     <body>
         <?php
-        require_once 'PHP/DB/users.class.php';
-        require_once 'PHP/DB/chatrooms.class.php';
+        require_once 'php/db/users.class.php';
+        require_once 'php/db/chatrooms.class.php';
         session_start();            
         $roomID = $_SESSION['roomID'];
         $username = $_SESSION['username'];
@@ -21,7 +21,7 @@ and open the template in the editor.
             Chatrooms::postMessage($username . ' has left the chatroom.', $roomID);
             Users::setRoomID(null);
         }
-        if(isset($_GET['roomClosed']) && $_GET['roomClosed']){
+        if(isset($_GET['roomClosed'])){
             echo 'The room has closed.';
         }
         echo '<a href="logout.php">Logout</a></br>';
@@ -30,7 +30,7 @@ and open the template in the editor.
         <div id="lobbyList"><?php include 'lobby_list.php' ?></div>
         
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
-        <script type="text/javascript" src="JS/loadLobby.js"></script>
+        <script type="text/javascript" src="js/loadLobby.js"></script>
         <script type="text/javascript">
             $(document).ready(function(){
                 //TODO: Don't forget to uncomment for lobby updates
