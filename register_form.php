858968eaffeecc962a15150f7d2b05a5a0f524c0
Julian Tith <julian.tith@gmail.com>
2012-06-03 23:33:05 -0700

    Rename form divs
    Rename form divs because they conflict with divs from login_form.php

--- register_form.php
+++ register_form.php
@@ -1,17 +1,16 @@
-<link rel="stylesheet" type="text/css" href="CSS/base.css"/>
 <link rel="stylesheet" type="text/css" href="CSS/registerForm.css"/>
 
 <div id="registerForm">
-    <form action="register.php" method="post">
+    <form action="index.php" method="post">
         <div id="register">Register</div> 
         <div id="username">Username:<br/>
-        <input id="username_input" type="text" name="username" value="" placeholder="Enter your username here"/>
-            </div>
+            <input id="username_input" type="text" name="regUsername" value="" placeholder="Enter your username here"/>
+        </div>
         <div id="password">Password:<br/>
-        <input id="password_input" type="password" name="password1" value="" placeholder="Enter your password here"/>       
+            <input id="password_input" type="password" name="regPassword1" value="" placeholder="Enter your password here"/>       
         </div>
         <div id="password">Password again:<br/>
-        <input id="password_input" type="password" name="password2" value="" placeholder="Enter your password here again"/>       
+            <input id="password_input" type="password" name="regPassword2" value="" placeholder="Enter your password here again"/>       
         </div>
         <input id="submit" type="submit" value="Register" />
     </form>
