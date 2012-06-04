f17c3fbdd3ca299797cc0513cdc2943470f13d8a
Julian Tith <julian.tith@gmail.com>
2012-06-03 23:37:41 -0700

    Merge login & register form into index & add register code
    Merge login & register form into index page.
    Add code to handle register request.

--- index.php
+++ index.php
@@ -1,33 +1,95 @@
 <?php 
-    require_once 'PHP/DB/users.class.php';
+    require_once 'php/db/users.class.php';
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Web Chat</title>
-        <link rel="stylesheet" type="text/css" href="CSS/base.css"/>
+        <link rel="stylesheet" type="text/css" href="css/base.css"/>      
+        <link rel="stylesheet" type="text/css" href="css/jquery.noty.css"/>
+        <link rel="stylesheet" type="text/css" href="css/noty_theme_default.css"/>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>        
+        <script type="text/javascript" src="js/jquery.noty.js"></script>
+        <script type="text/javascript" src="js/notyContainer.js"></script>
+        
     </head>
     <body>
         <?php
         session_start();
-        if (isset($_REQUEST['username']) && isset($_REQUEST['password'])){
+        
+        // Handle GET requests
+        if (isset($_GET['createUser'])) {
+            echo '<script type="text/javascript">successCreateUser()</script>';
+        }
+        
+        // Process user registration
+        if (isset($_POST['regUsername'])){
+            if(strlen($_POST['regUsername']) < 1 ) {
+                include 'login_form.php';
+                include 'register_form.php';       
+                echo '<script type="text/javascript">errorUsername();</script>';
+                die();
+            }
+
+            if(!isset($_POST['regPassword1']) || strlen($_POST['regPassword1']) < 4  ) {
+                include 'login_form.php';
+                include 'register_form.php';
+                echo '<script type="text/javascript">errorPasswordNotLongEnough();</script>';
+                die();
+            }
+            if(!isset($_POST['regPassword2']) || strlen($_POST['regPassword2']) < 4  ) {
+                include 'login_form.php';
+                include 'register_form.php';
+                echo '<script type="text/javascript">errorPasswordDoNotMatch();</script>';
+                die();
+            }
+
+            $username = $_POST['regUsername'];
+            $password1 = md5( $_POST['regPassword1'] );
+            $password2 = md5( $_POST['regPassword2'] );       
+
+            if($password1 != $password2) {
+                include 'login_form.php';
+                include 'register_form.php';
+                echo '<script type="text/javascript">errorPasswordDoNotMatch();</script>';
+                die();
+            }
+
+            $result = Users::register($username, $password1);
+            if ($result){
+                header( 'Location: index.php?createUser=true' );
+            }   
+            else if (mysql_errno() == 1062){
+                include 'login_form.php';
+                include 'register_form.php';
+                echo '<script type="text/javascript">errorUserAlreadyExist();</script>';
+                die();
+            }
+        }
+        
+        // Process user login
+        if (isset($_POST['username']) && isset($_POST['password'])){
             $username = $_REQUEST['username'];
             $password = md5($_REQUEST['password']);
             $login = Users::login($username, $password);           
             if(!$login) {
-                echo 'The username and password you entered do not match!';
+                echo '<script type="text/javascript">errorLogin();</script>';
             }
             else{
                 $_SESSION['userID'] = Users::getUserID($username);
                 $_SESSION['username'] = $username;
                 $_SESSION['roomID'] = NULL;
-                header( 'Location: lobby.php' );
+                header('Location: lobby.php');
             }            
         }
+        
+        // Show login and register form if no session
         if(!isset($_SESSION['username'])) {
             include 'login_form.php';
-        } else {
+            include 'register_form.php';
+        } 
+        else {
             echo '</br><a href="logout.php">logout</a>';
             header( 'Location: lobby.php' );
         }
