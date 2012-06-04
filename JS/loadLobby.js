ce9385f513770e5cfbd259b077757ecc2f3a4157
Julian Tith <julian.tith@gmail.com>
2012-06-03 23:40:46 -0700

    Remove files to fix folder name case

--- JS/loadLobby.js
+++
@@ -1,9 +0,0 @@
-function loadLobby(){
-        $.ajax({
-            url: "lobby_list.php",
-            cache: false,
-            success: function(html){
-                $("#lobbyList").html(html);
-            }
-        });
-}
