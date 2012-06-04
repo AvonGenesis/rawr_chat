633f40b512280a916360077efbe68ef7d1fd580e
Julian Tith <julian.tith@gmail.com>
2012-06-03 23:34:47 -0700

    Add input box titles

--- login_form.php
+++ login_form.php
@@ -1,15 +1,14 @@
 <link rel="stylesheet" type="text/css" href="CSS/loginForm.css"/>
 <div id="loginForm">
     <form action="index.php" method="post">
-        <div id="signin">Sign in</div> 
-        <input id="username_input" type="text" name="username" value="" placeholder="Enter your username"/>
+        <div id="signin">Sign in</div>
+        <div id="username">Username:</br>
+            <input id="username_input" type="text" name="username" value="" placeholder="Enter your username"/>
+        </div>
+        <div id="password">Password:</br>
         <input id="password_input" type="password" name="password" value="" placeholder="Enter your password"/>       
+        </div>
         <input id="submit" type="submit" value="Login" />
-        </br>
-        <a id="register" href="register_form.php">Register</a> 
     </form>
-
-
-
 </div>        
 
