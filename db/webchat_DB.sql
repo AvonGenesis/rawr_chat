e642638fc919df25a046c00231c04274b80fd73a
Julian Tith <julian.tith@gmail.com>
2012-06-05 14:54:06 -0700

    Add SQL table setup

---
+++ db/webchat_DB.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 05, 2012 at 09:50 PM
+-- Server version: 5.5.20
+-- PHP Version: 5.3.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `webchat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatlog`
+--
+
+CREATE TABLE IF NOT EXISTS `chatlog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roomID` int(11) NOT NULL,
+  `username` varchar(16) NOT NULL,
+  `picture` int(11) NOT NULL DEFAULT '0',
+  `text` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatrooms`
+--
+
+CREATE TABLE IF NOT EXISTS `chatrooms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roomCreatorID` int(11) NOT NULL,
+  `name` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `users` int(11) NOT NULL DEFAULT '0',
+  `deleted` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `roomID` int(11) DEFAULT NULL,
+  `picture` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
