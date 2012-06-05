7c37fb2ca8ebf7b1434f61024e35860b3f3733ab
Julian Tith <julian.tith@gmail.com>
2012-06-05 14:54:37 -0700

    Add picture selection to registration form

--- register_form.php
+++ register_form.php
@@ -12,6 +12,14 @@
         <div id="password">Password again:<br/>
             <input id="password_input" type="password" name="regPassword2" value="" placeholder="Enter your password here again"/>       
         </div>
+        <div id="picture">Picture:</br>
+            <img src="/images/1.png"/><input id="radio" type="radio" name="picture" value="1"/>
+            <img src="/images/2.png"/><input id="radio" type="radio" name="picture" value="2"/>
+            <img src="/images/3.png"/><input id="radio" type="radio" name="picture" value="3"/></br>
+            <img src="/images/4.png"/><input id="radio" type="radio" name="picture" value="4"/>
+            <img src="/images/5.png"/><input id="radio" type="radio" name="picture" value="5"/>
+            <img src="/images/6.png"/><input id="radio" type="radio" name="picture" value="6"/></br>
+        </div>
         <input id="submit" type="submit" value="Register" />
     </form>
 </div>
