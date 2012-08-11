3529897fb2d8fbfa423d1409c947d525260413bf
Julian Tith <julian.tith@gmail.com>
2012-08-10 20:18:07 -0700

    Modify to form to fit Twitter Bootstrap style

--- register_form.php
+++ register_form.php
@@ -1,24 +1,70 @@
-<link rel="stylesheet" type="text/css" href="CSS/registerForm.css"/>
-<div id="registerForm">
-    <form action="index.php" method="post">
-        <div id="register">Register</div> 
-        <div id="username">Username:<br/>
-            <input id="username_input" type="text" name="regUsername" value="" placeholder="Enter your username here"/>
+<div class="container">
+    <form action="" method="post" class="form-horizontal">
+        <fieldset>
+          <legend>Sign Up</legend>
+          <div class="control-group">
+            <label class="control-label" for="username">Username</label>
+            <div class="controls">
+              <input type="text" class="input-xlarge" id="username" name="username">
+              <p class="help-block">This will be your display name throughout the site.</p>
+            </div>
+          </div>
+          <div class="control-group">
+            <label class="control-label" for="password1">Password</label>
+            <div class="controls">
+              <input type="password" class="input-xlarge" id="password1" name="password1">
+              <p class="help-block">Must be at least 4 characters long.</p>
+            </div>
+          </div>
+          <div class="control-group">
+            <label class="control-label" for="password2">Verify Password</label>
+            <div class="controls">
+              <input type="password" class="input-xlarge" id="password2" name="password2">
+            </div>
+          </div>
+        <!--  
+        <div class="btn-group" data-toggle="buttons-radio">
+            <button class="btn"><img src="/images/1.png"/></button>
+            <button class="btn"><img src="/images/2.png"/></button>
+            <button class="btn"><img src="/images/3.png"/></button>
+            <button class="btn"><img src="/images/4.png"/></button>
+            <button class="btn"><img src="/images/5.png"/></button>
+            <button class="btn"><img src="/images/6.png"/></button>
         </div>
-        <div id="password">Password:<br/>
-            <input id="password_input" type="password" name="regPassword1" value="" placeholder="Enter your password here"/>       
-        </div>
-        <div id="password">Password again:<br/>
-            <input id="password_input" type="password" name="regPassword2" value="" placeholder="Enter your password here again"/>       
-        </div>
-        <div id="picture">Picture:</br>
-            <img src="/images/1.png"/><input id="radio" type="radio" name="picture" value="1"/>
-            <img src="/images/2.png"/><input id="radio" type="radio" name="picture" value="2"/>
-            <img src="/images/3.png"/><input id="radio" type="radio" name="picture" value="3"/></br>
-            <img src="/images/4.png"/><input id="radio" type="radio" name="picture" value="4"/>
-            <img src="/images/5.png"/><input id="radio" type="radio" name="picture" value="5"/>
-            <img src="/images/6.png"/><input id="radio" type="radio" name="picture" value="6"/></br>
-        </div>
-        <input id="submit" type="submit" value="Register" />
+        -->
+        <div class="control-group">
+            <label class="control-label">Avatar</label>
+            <div class="controls">
+                <label class="radio">
+                    <input type="radio" name="avatar" value="1">
+                    <img src="/images/1.png"/>
+                </label>
+                <label class="radio">
+                    <input type="radio" name="avatar" value="2">
+                    <img src="/images/2.png"/>
+                </label>
+                <label class="radio">
+                    <input type="radio" name="avatar" value="3">
+                    <img src="/images/3.png"/>
+                </label>
+                <label class="radio">
+                    <input type="radio" name="avatar" value="4">
+                    <img src="/images/4.png"/>
+                </label>
+                <label class="radio">
+                    <input type="radio" name="avatar" value="5">
+                    <img src="/images/5.png"/>
+                </label>
+                <label class="radio">
+                    <input type="radio" name="avatar" value="6">
+                    <img src="/images/6.png"/>
+                </label>
+            </div>
+        </div>          
+          <div class="form-actions">
+            <button type="submit" class="btn btn-primary">Sign Up</button>
+            <button class="btn">Cancel</button>
+          </div>
+        </fieldset>
     </form>
 </div>
