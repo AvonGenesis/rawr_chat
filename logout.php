a56937718b28712c4d94c0c3e797f1be473bbd64
Julian Tith <julian.tith@gmail.com>
2012-08-11 23:48:06 -0700

    Remove redirect timer and styling

--- logout.php
+++ logout.php
@@ -5,13 +5,11 @@
         <title>Logout</title>
         <script type="text/javascript">
         function delayer(){
-            window.location = "index.php"
+            window.location = "index.php?logout=successful"
         }
         </script>
-        <link rel="stylesheet" type="text/css" href="css/base.css"/>
-        <link rel="stylesheet" type="text/css" href="css/logout.css"/>
     </head>
-    <body onLoad="setTimeout('delayer()', 5000)">
+    <body onLoad="setTimeout('delayer()', 0)">
         <?php
         if( session_start() == true ) {
             require_once 'php/db/users.class.php';
@@ -22,10 +20,5 @@
             session_destroy();       
         }
         ?>
-        <div id="container">
-            You have successfully logged out!
-            </br>You will be redirected to the homepage in 5 seconds.
-            </br><a href="index.php">Click here if you do not wish to wait</a>
-        </div>
     </body>
 </html>
