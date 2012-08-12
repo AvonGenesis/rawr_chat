89c5f537e4b2aa15dfb6fc82153b86b914415995
Julian Tith <julian.tith@gmail.com>
2012-08-11 23:55:09 -0700

    Add code to handle left chatroom and fix table style

--- lobby.php
+++ lobby.php
@@ -1,32 +1,31 @@
 <?php
-require_once ('header.html');
 require_once ('php/db/users.class.php');
 require_once ('php/db/chatrooms.class.php');
-
-if (isset($_SESSION['userID']))
-{
+@session_start();
+if (isset($_SESSION['userID'])) {    
     $roomID = $_SESSION['roomID'];
     $username = $_SESSION['username'];
+    Chatrooms::postMessage($username . ' has left the chatroom.', $roomID);
+    $_SESSION['chatID'] = NULL;
+    Users::setRoomID(NULL);
 }
 
-if (isset($_GET['lobby'])) {
-    Chatrooms::postMessage($username . ' has left the chatroom.', $roomID);
-    $_SESSION['chatID'] = null;
-    Users::setRoomID(null);
-}
+require_once ('header.html');
 ?>
-<div class="container">
+<div class="container hero-unit" style="padding:20px;">
     <?php
     require_once 'php/db/chatrooms.class.php';
     Chatrooms::getChatroomList();
     ?>
 </div>
+<!-- Refresh Lobby Doesn't Work Anymore
 <script type="text/javascript">
     $(document).ready(function(){
         //TODO: Don't forget to uncomment for lobby updates
         setInterval(loadLobby, 1000);
     });
 </script>
+-->
 
 <?php
 require_once ('footer.html');
