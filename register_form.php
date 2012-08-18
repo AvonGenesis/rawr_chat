fbd260d504439ad8392105ab353300ef4f52d16f
Julian Tith <julian.tith@gmail.com>
2012-08-17 23:31:48 -0700

    Add jquery validate to form

--- register_form.php
+++ register_form.php
@@ -1,24 +1,31 @@
 <link rel="stylesheet" href="colorpicker/css/colorpicker.css" />
 <div class="container">
-    <form action="" method="post" class="form-horizontal">
+    <form action="" method="post" class="form-horizontal" id="register">
         <fieldset>
             <legend>Sign Up</legend>
             <div class="control-group">
-                <label class="control-label" for="username">Username</label>
+                <label class="control-label" for="input0">Username</label>
                 <div class="controls">
                     <input type="text" class="input-xlarge" id="username" name="username" />
-                    <p class="help-block">This will be your display name throughout the site.</p>
+                    <p class="help-block">This is your username that will be used to login.</p>
                 </div>
             </div>
             <div class="control-group">
-                <label class="control-label" for="password1">Password</label>
+                <label class="control-label" for="input0">Nickname</label>
+                <div class="controls">
+                    <input type="text" class="input-xlarge" id="nickname" name="nickname" />
+                    <p class="help-block">This will be your display name throughout the site. This can be changed later.</p>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="input0">Password</label>
                 <div class="controls">
                     <input type="password" class="input-xlarge" id="password1" name="password1" />
-                    <p class="help-block">Must be at least 4 characters long.</p>
+                    <p class="help-block">Must be at least 6 characters long.</p>
                 </div>
             </div>
             <div class="control-group">
-                <label class="control-label" for="password2">Verify Password</label>
+                <label class="control-label" for="input0">Verify Password</label>
                 <div class="controls">
                     <input type="password" class="input-xlarge" id="password2" name="password2" />
                 </div>
@@ -34,10 +41,10 @@
                 </div>
                 -->
             <div class="control-group">
-                <label class="control-label" for="colorpicker">Chat bubble color</label>
+                <label class="control-label" for="input0">Chat bubble color</label>
                 <div class="controls">
                     <div class="input-append color" data-color="#53998C" id="colorpicker" data-color-format="hex">
-                        <input type="text" class="span2" value="" readonly name="color" />
+                        <input type="text" class="span2" value="#53998C" readonly name="color" />
                         <span class="add-on"><i style="background-color: #53998C"></i></span>
                     </div>
                     <p class="help-block">Pick a color for your chat bubble, this can be changed later in the settings.</p>
@@ -87,4 +94,48 @@
             format: 'hex'
         });
     });
+</script>
+<script type="text/javascript" src="http://jzaefferer.github.com/jquery-validation/jquery.validate.js"></script>
+<script>
+$(document).ready(function()
+{
+$("#register").validate({
+rules:{
+username:{required:true},
+nickname:{required:true},
+colorpicker:{required:true,minlength: 7},
+password1:{required:true,minlength: 6},
+password2:{required:true,minlength: 6, equalTo:"#password1"}
+},
+
+messages:{
+username:{
+required:"Enter your username"},
+nickname:{
+required:"Enter your nickname"},
+colorpicker:{
+required:"Select a color",
+minlength:"Color must be at least 6 characters long"},
+password1:{
+required:"Enter your password",
+minlength:"Password must be minimum 6 characters"},
+password2:{
+required:"Enter confirm password",
+minlength:"Password must be minimum 6 characters",
+equalTo:"Password and Confirm Password must match"}
+},
+
+errorClass: "help-inline",
+errorElement: "span",
+highlight:function(element, errorClass, validClass)
+{
+$(element).parents('.control-group').addClass('error');
+},
+unhighlight: function(element, errorClass, validClass)
+{
+$(element).parents('.control-group').removeClass('error');
+$(element).parents('.control-group').addClass('success');
+}
+});
+});
 </script>
