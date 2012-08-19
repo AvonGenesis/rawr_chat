72d4ab91827d8fc8366434032d215f72e2c903d4
Julian Tith <julian.tith@gmail.com>
2012-08-19 00:50:05 -0700

    Add javascript, styling, and form for color change

--- settings.php
+++ settings.php
@@ -11,7 +11,25 @@ if (isset($_POST['nickname'])) {
     echo Users::changeNickname($_SESSION['username'], $_POST['nickname']);
 }
 ?>
-
+<link rel="stylesheet" href="colorpicker/css/colorpicker.css" />
+<style type="text/css">
+#chatBackground {
+    background-image: url('/images/background.png');
+    background-size: 1px 40px;
+    background-repeat:repeat-x;
+    width:80%;
+    border-style: solid;
+    border-radius: 15px;
+    border-width: 5px;
+    border-color: #ffffff;
+    height: 30px;
+    font-size: 20px;
+    padding-top: 10px;
+    position:relative;
+    top: -55px;
+    left: 75px;
+}
+</style>
 <div class="container">
     <div class="row">
         <div class="span6">
@@ -49,11 +67,35 @@ if (isset($_POST['nickname'])) {
                 </div>
                 <button type="submit" class="btn btn-primary" style="display:block; margin-right:auto; margin-left: auto;">Change Nickname</button>
             </form>
-            
+            <form action="settings.php" method="post" class="well form-horizontal">
+                <label><h3><center>Change Color</center></h3></label>
+                <div class="control-group">
+                    <label class="control-label" for="input01">Color</label>
+                    <div class="controls">
+                        <input type="text" class="span1" value="<?php echo '#' . $_SESSION['color'];?>" id="colorpicker" />
+                    </div>
+                </div>
+                <div id="chatExample" style="background-color: #000000; margin-bottom: 10px; padding: 10px; height: 58px;">
+                    <div id="chatIcon"><img src="/images/1.png"/></div>
+                    <div id="chatBackground" style="background-color: <?php echo '#' . $_SESSION['color'];?>">
+                        <div style="color: #ffffff"><center>TEXT GOES HERE</center></div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary" style="display:block; margin-right:auto; margin-left: auto;">Change Color</button>
+            </form>
         </div>
     </div>
 </div>
-
+<script src="colorpicker/js/bootstrap-colorpicker.js"></script>
+<script>
+    $(function(){
+        var bodyStyle = $('#chatBackground')[0].style;
+        $('#colorpicker').colorpicker().on('changeColor', function(ev){
+            bodyStyle.backgroundColor = ev.color.toHex();
+        });
+    });
+    
+</script>
 <?php
 require_once ('footer.html');
 ?>
