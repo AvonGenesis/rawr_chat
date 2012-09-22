0c36c24dbfa4befdc759a6316111221e351d4ed2
Julian Tith <julian@juliantith.com>
2012-09-22 16:04:28 -0700

    Remove uneeded HTML from signout

--- logout.php
+++ logout.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Logout</title>
-        <script type="text/javascript">
-        function delayer(){
-            window.location = "index.php?logout=successful"
-        }
-        </script>
-    </head>
-    <body onLoad="setTimeout('delayer()', 0)">
-        <?php
-        if (session_start() == true) {
-            include_once 'php/db/users.class.php';
-            $username = $_SESSION['username'];
-            $roomID = (int)$_SESSION['roomID'];
-            $text = $username . ' has left the chatroom.';
-            Users::logout($username, $roomID, $text);
-            session_destroy();       
-        }
-        ?>
-    </body>
-</html>
+<?php
+if (session_start() == true) {
+    include_once 'php/db/users.class.php';
+    $username = $_SESSION['username'];
+    $roomID = (int)$_SESSION['roomID'];
+    $text = $username . ' has left the chatroom.';
+    Users::logout($username, $roomID, $text);
+    session_destroy();
+    header('Location: index.php?logout=successful');    
+}
+?>
