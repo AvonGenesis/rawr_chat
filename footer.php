59cf4fe282a456420816e10e1e8ac3de69aeb141
Julian Tith <julian@juliantith.com>
2013-02-18 18:58:27 -0800

    Add bootstrap files

--- footer.php
+++ footer.php
@@ -11,5 +11,7 @@
                 </div>
             </div>
         </footer> -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.js"></script>
     </body>
 </html>
